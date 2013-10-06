5c151a3e60a25c86d7e1226c12ef81d595319f89
donhmai <donmai@berkeley.edu>
2013-10-05 18:20:55 -0700

    rip_router push

--- rip_router.py
+++ rip_router.py
@@ -189,6 +189,118 @@ class SmartRIPRouter (RIPRouter):
       #Send only to the newly connected neighbor a DV ONLY IF LINK IS UP
       self._send_out_distance_vector([source])
 
+class LSRouter (Entity):
+  def __init__(self):
+    self.graph = Graph()
+    self.graph.add_node(self)
+    self.forwarding_table = dict()
+    self.port_lookup = dict()
+    self.list_of_hosts = set()
+    self.processed_packets = set()
+
+  def handle_rx(self, packet, port):
+    if isinstance(packet, DiscoveryPacket):
+      self._handle_discovery(packet, port)
+    elif isinstance(packet, LinkStatePacket):
+      self._handle_linkstate_update(packet, port)
+    else:
+      self._handle_data(packet, port)
+
+  def _handle_discovery(self, packet, port):
+    source = packet.src # source is another word for neighbor
+    new_edge = (source, self)
+    if packet.is_link_up:
+      self.graph.add_node(source)
+      self.graph.add_edge(new_edge, 1)
+      if isinstance(source, HostEntity) and source not in self.list_of_hosts:
+        self.list_of_hosts.add(source)
+      self.port_lookup[source] = port
+    else:
+      # Nodes may become isolated in a router's graph after this
+      self.graph.del_node(new_edge, 1)
+      if isinstance(source,HostEntity) and source in self.list_of_hosts:
+        self.list_of_hosts.remove(source)
+      try:
+        del self.port_lookup[source]
+      except KeyError as e:
+        print "Why you remove %s when I DONT HAVE IT. QQ" % source
+        print e
+
+    # recompute forwarding table
+    self._update_forwarding_table()
+    # flood packet updates
+    self._send_linkstate_update(packet, port) # Need to send link state update to all neighbors
+  
+
+  def _send_linkstate_update(self, packet, port=None):
+    if isinstance(packet, DiscoveryPacket):
+      # This clause only executes if a router is responding to a DiscoveryPacket
+      # Creation of the LinkStatePacket occurs here
+      packet_to_be_sent = LinkStatePacket(packet.src, self, packet.is_link_up)
+    else: # packet arg is type LinkStatePacket
+      packet_to_be_sent = packet
+    self.send(packet_to_be_sent, port, flood=True) # port's value is obtained from method header
+
+  def _handle_linkstate_update(self, packet, port):
+    # first, check for duplicate linkstate update
+    # For some reason, the same packet is being stored multiple times
+    # in self.processed_packets
+    # Check out the 3 print statements in this method. 'hi' is
+    # never printed, and the hashset keeps incrementing.
+    print(len(self.processed_packets))
+    print(packet)
+    if packet not in self.processed_packets:
+      self.processed_packets.add(packet)
+
+      # process update
+      new_edge = (packet.node1, packet.node2)
+      if packet.is_link_up:
+        self.graph.add_node(new_edge[0])
+        self.graph.add_node(new_edge[1])
+        self.graph.add_edge(new_edge, 1)
+      else:
+        self.graph.del_edge(new_edge)
+
+      # recompute forwarding table
+      self._update_forwarding_table()
+
+      # flood packet updates
+      self._send_linkstate_update(packet, port) # Need to send link state update to all neighbors, except for neighbor that sent packet to current router 
+    else:
+      print('hi')
+
+  def _update_forwarding_table(self):
+    intermediate_dict = self.graph.shortest_paths(self)
+    self.forwarding_table = dict()
+    for host in self.list_of_hosts:
+      best_neighbor = intermediate_dict[host][0][0]
+      self.forwarding_table[host] = best_neighbor
+
+  def _handle_data(self, packet, port):
+    destination = packet.dst
+    try:
+      best_neighbor = self.forwarding_table[destination]
+      self.send(packet, port=self.port_lookup[best_neighbor])
+    except KeyError as e:
+      print "There is no route to this destination %s via this router, period." % destination
+      print e
+
+
+class LinkStatePacket (Packet):
+  """
+  A "link state packet" (Packet):
+  Contains information on an edge and whether or not that edge
+  went up or down. Sending out this packet is effectively
+  equivalent to sending out a list of neighbors of a source packet A
+  after a single link has gone up or down involving A.
+  """
+
+  def __init__(self, node1, node2, link_status):
+    Packet.__init__(self)
+    self.node1 = node1
+    self.node2 = node2
+    self.is_link_up = link_status
+
 '''
 EC3
 '''
@@ -202,7 +314,7 @@ class Graph:
     if not self.adjaceny_list.has_key(node):
       self.adjaceny_list[node] = set()
     else:
-      print "Node already added!"
+      print "Node %s already added! %s is source" % (node, self)
 
   def add_edge(self, edge, wt=1):
     node1 = edge[0]
